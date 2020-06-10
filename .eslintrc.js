4f6bae95aed327c091d27b7db2d506e86317850f
jackpalaia <jack@jackpalaia.com>
2020-06-09 22:21:11 -0400

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,29 @@
 module.exports = {
-  "env": {
-    "commonjs": true,
-        "es2020": true,
-        "node": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
+  'env': {
+    'commonjs': true,
+    'es2020': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 11
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
     ],
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 11
-    },
-    "plugins": [
-        "react"
+    'linebreak-style': [
+      'error',
+      'windows'
     ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "never"
-        ]
-    }
-};
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'never'
+    ]
+  }
+}
