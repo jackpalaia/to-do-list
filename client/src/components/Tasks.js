497c46286ce8b2cd252d35fee77dd57f52f55e91
jackpalaia <jack@jackpalaia.com>
2020-06-10 21:30:17 -0400

    removed console.log

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -4,7 +4,6 @@ import Task from '../components/Task'
 const Tasks = ({ taskList }) => (
   <div>
     <ul>
-      {console.log(typeof(taskList))}
       {taskList.map((task, i) => <Task key={i} task={task} />)}
     </ul>
   </div>
