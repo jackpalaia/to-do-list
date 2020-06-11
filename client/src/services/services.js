d219fe46fa7d16803e42182ed3acb7c17ce9904f
jackpalaia <jack@jackpalaia.com>
2020-06-10 22:06:57 -0400

    added create, remove, update

--- client/src/services/services.js
+++ client/src/services/services.js
@@ -4,5 +4,14 @@ const baseUrl = '/api/tasks'
 const getAll = async () => {
   return await axios.get(baseUrl).data
 }
+const create = async task => {
+  return await axios.post(baseUrl, task).data
+}
+const remove = async id => {
+  return await axios.delete(`${baseUrl}/${id}`)
+}
+const update = async (id, task) => {
+  return await axios.put(`${baseUrl}/${id}`, task)
+}
 
-export default { getAll }
+export default { getAll, create, remove, update }
