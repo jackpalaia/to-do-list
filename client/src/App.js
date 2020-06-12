6e16ad4096b1b05d79faa973e3e5ecae0cdd0dc1
jackpalaia <jack@jackpalaia.com>
2020-06-11 20:21:46 -0400

    useEffect get all notes initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Input from './components/Input'
-import service from './services/services'
+import taskService from './services/services'
 import Tasks from './components/Tasks'
 
 const App = () => {
   const [tasks, setTasks] = useState([])
   const [inputValue, setInputValue] = useState('')
+
+  useEffect(async () => {
+    setNotes(taskService.getAll())
+  }, [])
 
   const handleFormSubmit = e => {
     e.preventDefault()
